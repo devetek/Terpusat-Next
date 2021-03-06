4bfe49ed3f0afaff10c85977abc78dd4af2cabc0
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-03-06 21:35:20 +0700

    feat(repo): move constant to env variable, to make it secret

--- libs/graphql.tsx
+++ libs/graphql.tsx
@@ -1,7 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const ENDPOINT = 'https://api-ap-northeast-1.graphcms.com/v2/ckk8wdyz26qm301xi40i59daq/master';
-const GQL_INSTANCE = new GraphQLClient(ENDPOINT);
+const GQL_INSTANCE = new GraphQLClient(process.env.ENDPOINT);
 
 const graphqlClient = async (queries: any, variables: any = null) => {
     try {
